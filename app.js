e75eb99e221c76e4bebd3ca66be3d2ab92799a53
abcd1234567890 <asahi114711@icloud.com>
2025-08-13 18:10:05 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 　let qrReader;
 
 　const GAS_URL = "https://script.google.com/macros/s/AKfycbygpqW4VYNm__Wip39CwAwoyitrTi4CPAg4N6lH7WPOPkcU37LbzS2XiNn-xvWzEI84/exec";
-  const ENDPOINT = "https://script.google.com/macros/s/AKfycbzPp8txbAQKRbIl_vzsf-S5v4D78lx4fFttikHh_Cv9bsUBDHWmQAliTFWTCYIujAug/exec";
+  const ENDPOINT = "https://script.google.com/macros/s/AKfycbwPi7zqGkT-AebQ2YWMoMxHhK-MmQ4I3oDm9TPooGYH9kYoow_-RHE8Dia8KxWZUyVA/exec";
   const gas_URL = "https://script.google.com/macros/s/AKfycbzPUOz4eCsZ7RVm4Yf_VPu1OC5nn2yIOPa5U-tT7ZMHpw0FRNsHaqovbX7vSaEHjPc/exec";
   const SECRET = 'kosen-brain-super-secret';
 　const secret = 'kosen'
