317903e92f834466796ee6501b541f721009ca38
PJa-Thus <shirokumaneko335@gmail.com>
2025-09-26 09:16:39 +0900

    d

--- app.js
+++ app.js
@@ -88,6 +88,8 @@ function applyTheme() {
   });
 }
 
+
+const themeSection = document.getElementById("themeSection");
 function v() {
   themeSection.style.display = themeSection.style.display === "none" ? "block" : "none";
 }
@@ -96,15 +98,11 @@ function v() {
 // イベントバインド
 // =====================
 window.addEventListener("DOMContentLoaded", ()=>{
-  const themeSection = document.getElementById("themeSection");
   const openThemeBtn = document.getElementById("openThemeBtn");
   const closeThemeBtn = document.getElementById("closeThemeBtn");
   const applyThemeBtn = document.getElementById("applyThemeBtn");
   sidebar = document.getElementById("sidebar");
-  // 開閉ボタン
-  openThemeBtn.addEventListener("click", ()=>{
-    themeSection.style.display = themeSection.style.display === "none" ? "block" : "none";
-  });
+  // 閉ボタン
   closeThemeBtn.addEventListener("click", ()=>{
     themeSection.style.display = "none";
   });
